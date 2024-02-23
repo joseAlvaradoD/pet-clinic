dfa1b5451f4993abae91437f7384553e1bd42a16
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-23 11:50:14 -0600

    Impl Map based services. closes #6

---
+++ pet-clinic-data/src/main/java/jjad/springframework/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,37 @@
+package jjad.springframework.petclinic.services.map;
+
+import jjad.springframework.petclinic.model.Vet;
+import jjad.springframework.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap
+        extends AbstractMapService<Vet, Long>
+        implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+}
