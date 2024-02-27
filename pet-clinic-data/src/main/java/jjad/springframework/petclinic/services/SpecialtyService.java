946ccaac9c73375c55cc8f1858b3f8ef1948551b
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-27 17:34:20 -0600

    adding specialty map service impl. closes #51

---
+++ pet-clinic-data/src/main/java/jjad/springframework/petclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package jjad.springframework.petclinic.services;
+
+import jjad.springframework.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long>{
+}
