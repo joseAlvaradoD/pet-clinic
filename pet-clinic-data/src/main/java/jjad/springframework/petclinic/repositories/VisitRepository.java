67289802cf6b11f45871696e04ae572d05027924
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-29 17:39:52 -0600

    adding Visit Map and Spring Data JPA Services. closes #85 and closes #86

--- pet-clinic-data/src/main/java/jjad/springframework/petclinic/repositories/VisitRepository.java
+++ pet-clinic-data/src/main/java/jjad/springframework/petclinic/repositories/VisitRepository.java
@@ -1,6 +1,7 @@
 package jjad.springframework.petclinic.repositories;
 
+import jjad.springframework.petclinic.model.Visit;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VisitRepository extends CrudRepository<VisitRepository, Long> {
+public interface VisitRepository extends CrudRepository<Visit, Long> {
 }
---
+++ pet-clinic-data/src/main/java/jjad/springframework/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package jjad.springframework.petclinic.services.map;
+
+import jjad.springframework.petclinic.model.Visit;
+import jjad.springframework.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService
+        extends AbstractMapService<Visit, Long>
+        implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if(object.getPet() == null || object.getPet().getOwner() == null ||
+                object.getPet().getId() == null || object.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
