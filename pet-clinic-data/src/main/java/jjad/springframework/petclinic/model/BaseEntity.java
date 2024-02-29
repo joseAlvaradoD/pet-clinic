e2b66ff3d7e3c7e19d5f815e0e0dc9cbe21b5637
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-02-29 15:34:59 -0600

    Creating base entity

--- pet-clinic-data/src/main/java/jjad/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/jjad/springframework/petclinic/model/BaseEntity.java
@@ -1,5 +1,9 @@
 package jjad.springframework.petclinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +11,11 @@ import java.io.Serializable;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 }
