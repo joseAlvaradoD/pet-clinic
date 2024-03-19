ae1aa6ef36fbb23533a63591e2e417b4e7342503
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-03-19 11:41:44 -0600

    adding save and update visit. closes #95

--- pet-clinic-data/src/main/java/jjad/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/jjad/springframework/petclinic/model/Pet.java
@@ -21,7 +21,9 @@ public class Pet extends BaseEntity{
         this.petType = petType;
         this.owner = owner;
         this.birthDate = birthDate;
-        this.visits = visits;
+        if(visits != null){
+            this.visits = visits;
+        }
     }
 
     @Column(name = "name")
