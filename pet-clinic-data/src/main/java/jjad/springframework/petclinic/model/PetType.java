1f1e8c36d00146aa890f6d0d87174228e2550cf7
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-03-19 11:02:48 -0600

    adding create and update pet. closes #95

--- pet-clinic-data/src/main/java/jjad/springframework/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/jjad/springframework/petclinic/model/PetType.java
@@ -4,17 +4,28 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 
 }
