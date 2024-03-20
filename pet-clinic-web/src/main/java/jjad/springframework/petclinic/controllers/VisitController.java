7145838c0c76cfa59d8282d4b64f110b4ccfc317
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-03-19 20:42:26 -0600

    adding Visit Controller Tests, closes #117

--- pet-clinic-web/src/main/java/jjad/springframework/petclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/jjad/springframework/petclinic/controllers/VisitController.java
@@ -11,6 +11,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+
 @Controller
 public class VisitController {
 
@@ -25,6 +28,13 @@ public class VisitController {
     @InitBinder
     public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     /**
